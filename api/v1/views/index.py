889ed4221c56daf5e318da2f39b3f882b49a9a0e
Pinerealm <osalami09@gmail.com>
2023-08-02 02:52:37 +0300

    Implement the stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """The index module
 """
-from api.v1.views import app_views
 from flask import jsonify
+
+from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -10,3 +12,15 @@ def status():
     """Returns a JSON string with the status
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Returns a JSON string with the stats
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
